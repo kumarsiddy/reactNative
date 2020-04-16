8d6e608481a19d6f5d33f99bf6f0d2bc4940cc6e
kumarsiddy <kumarsiddy@gmail.com>
2020-04-16 17:03:46 +0530

    Initial Commit

---
+++ App.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import {Button, StyleSheet, Text, View} from 'react-native';
+
+export default function App() {
+
+    let onButtonPressed = () => {
+        alert('Button has been clicked!!')
+    }
+
+    return (
+        <View style={styles.container}>
+            <Text>Hello Siddhartha!!</Text>
+            <Button title={'Button'} onPress={onButtonPressed}/>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+});
